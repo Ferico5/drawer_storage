d8e1bb96b177d6cf6c16c19f74674d7dbb3b3176
ferico <huangferico@gmail.com>
2025-05-19 14:28:57 +0700

    setup tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,72 @@
+// eslint-disable-next-line no-unused-vars
+import { createBrowserRouter, RouterProvider, Outlet, Navigate } from 'react-router-dom';
+// import ProtectedRoute from '../components/ProtectedRoute';
+
+// import pages
+// import Auth from '../pages/Auth';
+import Dashboard from '../pages/Dashboard';
+
+// import components
+// import Header from '../components/Header';
+
+// // Import context
+// import { AuthProvider } from '../auth/AuthContext';
+// import ReservedBook from '../pages/ReservedBook';
+
+const Layout = () => {
+  return (
+    <>
+      {/* <Header /> */}
+      <Outlet />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      // {
+      //   path: '/',
+      //   element: <Navigate to="/dashboard" replace />,
+      // },
+      // {
+      //   path: '/auth',
+      //   element: <Auth />,
+      // },
+      {
+        path: '/dashboard',
+        element: (
+          // <ProtectedRoute>
+            <Dashboard />
+          // </ProtectedRoute>
+        ),
+      },
+      // {
+      //   path: '/server-error',
+      //   element: (
+      //     <ProtectedRoute>
+      //       <ServerError />
+      //     </ProtectedRoute>
+      //   ),
+      // },
+      // {
+      //   path: '*',
+      //   element: <PageNotFound />,
+      // },
+    ],
+  },
+]);
+
+const App = () => {
+  return (
+    <div>
+      {/* <AuthProvider> */}
+        <RouterProvider router={router} />
+      {/* </AuthProvider> */}
+    </div>
+  );
+};
+
+export default App;
