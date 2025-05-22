2d24a08ab53eeb6bdb362a6529ae685090ea4e08
ferico <huangferico@gmail.com>
2025-05-22 10:03:44 +0700

    show name and nav after login

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,37 +1,42 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../auth/AuthContext.jsx';
 
 const Header = () => {
+  const { token, user, logout } = useAuth();
+  const navigate = useNavigate();
+
   return (
     <div className="w-full bg-[#1E1E2E] text-white p-5 text-lg flex justify-between">
       {/* nama usaha */}
       <div>Laci Penyimpanan</div>
       {/* navbar */}
-      <div>
-        <nav>
-          <ul className="flex gap-10">
-            <li>
-              <Link to="/dashboard" className="hover:text-gray-400">
-                Tabel Laci
-              </Link>
-            </li>
-            <li>
-              <Link to="/insert-item" className="hover:text-gray-400">
-                Tambahkan Barang
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      {token && (
+        <div>
+          <nav>
+            <ul className="flex gap-10">
+              <li>
+                <Link to="/dashboard" className="hover:text-gray-400">
+                  Tabel Laci
+                </Link>
+              </li>
+              <li>
+                <Link to="/insert-item" className="hover:text-gray-400">
+                  Tambahkan Barang
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      )}
 
       <div className="flex items-center">
         {/* Nama User */}
-        {/* {token && user && <p className="font-medium mr-10">{user?.full_name}</p>}
+        {token && user && <p className="font-medium mr-10">{user?.nama}</p>}
         {token && user && (
           <button onClick={() => logout(navigate)} className="bg-red-500 px-3 py-1 rounded-md hover:bg-red-600 hover:cursor-pointer">
             Logout
           </button>
-        )} */}
-        <p>Nama</p>
+        )}
       </div>
     </div>
   );
